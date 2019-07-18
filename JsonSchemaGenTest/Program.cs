67317b66f073362188596a6c816774f712985247
John Baro <johnbaro@gmail.com>
2019-07-18 10:32:35 +1000

    IC

--- JsonSchemaGenTest/Program.cs
+++ JsonSchemaGenTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NJsonSchema;
+using System;
 
 namespace JsonSchemaGenTest
 {
@@ -6,7 +7,79 @@ namespace JsonSchemaGenTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            /*
+             * Test project to illustrate how to manually generate a JSON schema
+This is our test json which should validate against our schema
+
+[
+    {
+        "id":1,
+        "value":"one",
+        "enabled":false
+    },
+    {
+        "id":2,
+        "value":"two",
+        "enabled":true
+    }
+]
+
+This is what our schema should look like
+{
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "type": "array",
+    "items": {
+        "$ref": "#/definitions/DataRowElement"
+    },
+    "definitions": {
+        "DataRowElement": {
+            "type": "object",
+            "additionalProperties": false,
+            "properties": {
+                "id": {
+                    "type": "integer"
+                },
+                "value": {
+                    "type": "string"
+                },
+                "enabled": {
+                    "type": "boolean"
+                }
+            },
+            "required": [
+                "enabled",
+                "id",
+                "value"
+            ],
+            "title": "DataRowElement"
+        }
+    }
+}
+             */
+
+            var objectSchema = new JsonSchema();
+            objectSchema.Type = JsonObjectType.Object;
+            objectSchema.Title = "DataRowElement";
+            objectSchema.Properties["id"] = new JsonSchemaProperty { Type = JsonObjectType.Integer };
+            objectSchema.Properties["value"] = new JsonSchemaProperty { Type = JsonObjectType.String };
+            objectSchema.Properties["enabled"] = new JsonSchemaProperty { Type = JsonObjectType.Boolean };
+
+            var schemaJson = objectSchema.ToJson();
+            Console.WriteLine(schemaJson);
+            Console.WriteLine("All good so far, this is our reference schema for our array elements");
+
+            var arraySchema = new JsonSchema();
+            arraySchema.Type = JsonObjectType.Array;
+            arraySchema.Definitions["DataRowElement"] = objectSchema;
+
+            schemaJson = arraySchema.ToJson();
+            Console.WriteLine(schemaJson);
+            Console.WriteLine("This is where we fall down, how do we reference our `DataRowElement` schema definition as our array `items` reference?");
+
+
+            Console.WriteLine("Any key to exit");
+            Console.ReadKey();
         }
     }
 }
